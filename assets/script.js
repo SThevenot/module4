1951e73ff354c144814c1456e0c704a7e40dd720
SThevenot <sktmh007@gmail.com>
2022-08-10 12:21:55 -0500

    fixed the timer

--- assets/script.js
+++ assets/script.js
@@ -9,31 +9,44 @@ var div3 = document.querySelector(".box3");
 var div4 = document.querySelector(".box4");
 var div5 = document.querySelector(".box5");
 var timer = document.querySelector(".timer");
-const pfalse = document.querySelectorAll(".pfalse");
+var pfalse = document.querySelectorAll(".pfalse");
 var timeLeft = 60;
 var finalScoreDiv = document.querySelector(".finalScore");
 var finalTime = document.querySelector(".finalTime");
-
+var p5true = document.querySelector("#p5true");
+var p5false = document.querySelectorAll(".p5false");
 //event listeners
 startButton.addEventListener("click", showDiv1, countdown);
 
 pfalse.forEach((element) => {
   element.addEventListener("click", (e) => {
-    console.log("wrong clicked");
+    console.log("wrng clicked");
     timeLeft -= 10;
   });
 });
 
+p5false.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    console.log("wrng clicked");
+    timeLeft -= 10;
+    pause_timer();
+  });
+});
+
+
 //functions
 function countdown() {
+  timeLeft;
   var timeInterval = setInterval(function () {
     if (timeLeft > 1) {
       timer.textContent = timeLeft + " seconds remaining";
       timeLeft--;
     } else if (timeLeft === 1) {
       timer.textContent = timeLeft + " second remaining";
+      timeLeft--;
     } else {
       timer.textContent = "Time's up!";
+      showScore();
       //add in line of code to take user to scoreboard stuff when timer hits 0
     }
   }, 1000);
@@ -70,23 +83,25 @@ function showDiv5() {
 }
 
 //local storage such and score
-function pause_timer() {
-  var currentTime = timeLeft;
-  finalTime.textContent = "Your score is " + currentTime;
-  timer.style.display = "none";
-  finalTime.style.display = "flex";
-}
-
 function showScore() {
   div5.style.display = "none";
   finalScoreDiv.style.display = "flex";
 }
 
-function scoreboard() {
-  console.log(userInfo);
+function pause_timer() {
+  var currentTime = timeLeft;
+  finalTime.textContent = "Your score is " + currentTime;
+  timer.style.display = "none";
+  finalTime.style.display = "flex";
+  console.log(currentTime);
+  console.log(timeLeft);
 }
 
-localStorage.setItem("initials", initials);
-console.log(initials);
-localStorage.setItem("score", score);
-console.log(score);
+// function scoreboard() {
+//   console.log(userInfo);
+// }
+
+// localStorage.setItem("initials", initials);
+// console.log(initials);
+// localStorage.setItem("score", score);
+// console.log(score);
