63840aced0755033b3d79be8f8f9c2fa13cfb757
SThevenot <sktmh007@gmail.com>
2022-08-11 09:51:40 -0500

    fixed some minor errors in js

--- assets/script.js
+++ assets/script.js
@@ -8,9 +8,8 @@ var div2 = document.querySelector(".box2");
 var div3 = document.querySelector(".box3");
 var div4 = document.querySelector(".box4");
 var div5 = document.querySelector(".box5");
-var timer = document.querySelector(".timer");
+var timer = document.querySelector("#timer");
 var pfalse = document.querySelectorAll(".pfalse");
-var timeLeft = 60;
 var finalScoreDiv = document.querySelector(".finalScore");
 var finalTime = document.querySelector(".finalTime");
 var p5true = document.querySelector("#p5true");
@@ -20,9 +19,33 @@ var p5true = document.querySelector("#p5true");
 var scoreDisplay = document.querySelector("#scoreDisplay");
 var scoreboard = document.querySelector("#scoreboard");
 var currentTime = {};
+var retryBtn = document.querySelector("#retryBtn");
+var time;
+startAgainButton = document.querySelector("#startAgainBtn");
 
 //event listeners
+//buttons
 startButton.addEventListener("click", showDiv1, countdown);
+
+submitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (initials === "") {
+    console.log("yes");
+    alert("error, initials cannot be blank");
+    showScore();
+  } else {
+    alert("success, saved successfully");
+    var initials = document.querySelector("#initials").value;
+    localStorage.setItem("initials", initials);
+    renderScoreboard();
+  }
+});
+
+retryBtn.addEventListener("click", reveal);
+
+startAgainButton.addEventListener("click", showDiv1, resetTimer);
+
+//answer option event listeners
 
 pfalse.forEach((element) => {
   element.addEventListener("click", (e) => {
@@ -41,43 +64,15 @@ p5false.forEach((element) => {
 
 p5true.addEventListener("click", pause_timer, showScore);
 
-submitBtn.addEventListener("click", function(event) {
-  event.preventDefault();
-  var initials = document.querySelector("#initial").value;
-  if (initials === "") {
-    console.log("yes");
-    alert("error, initials cannot be blank");
-  } else {
-    alert("success, saved successfully");
-    localStorage.setItem("initials", initials);
-    renderScoreboard();
-  }
-  })
-  
 
-//functions
-function countdown() {
-  timeLeft;
-  var timeInterval = setInterval(function () {
-    if (timeLeft > 1) {
-      timer.textContent = timeLeft + " seconds remaining";
-      timeLeft--;
-    } else if (timeLeft === 1) {
-      timer.textContent = timeLeft + " second remaining";
-      timeLeft--;
-    } else {
-      timer.textContent = "Time's up! Your score is 0";
-      timer.style.margin = 0;
-      showScore();
-
-      //add in line of code to take user to scoreboard stuff when timer hits 0
-    }
-  }, 1000);
-}
+//functions for hiding/showing questions
 
 function showDiv1() {
   div1.style.display = "flex";
   startButton.style.display = "none";
+  startAgainButton.style.display = "none";
+  scoreboard.style.display = "none";
+  retryBtn.style.display = "none";
   countdown();
 }
 
@@ -105,27 +100,73 @@ function showDiv5() {
   div5.style.display = "flex";
 }
 
-//local storage such and score
+//input initials to save score to local storage
 function showScore() {
   div5.style.display = "none";
   finalScoreDiv.style.display = "flex";
 }
 
+//pauses and resets time and saves time on clock as score
 function pause_timer() {
   var currentTime = timeLeft;
   finalTime.textContent = "Your score is " + currentTime;
   timer.style.display = "none";
   finalTime.style.display = "flex";
-   localStorage.setItem("currentTime", JSON.stringify(currentTime));
+  localStorage.setItem("currentTime", JSON.stringify(currentTime));
+  clearInterval(time);
 }
 
-
-function renderScoreboard() { 
-var initials = localStorage.getItem("initials");
-var retrieveScore = localStorage.getItem("currentTime");
-scoreDisplay.textContent = retrieveScore + " - " + initials;
-finalScoreDiv.style.display = "none";
-scoreboard.style.display = "flex";
-return;
+//countdown function
+function countdown() {
+  timeLeft = 60;
+  time = setInterval(function () {
+    if (timeLeft > 1) {
+      timer.textContent = timeLeft + " seconds remaining";
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      timer.textContent = timeLeft + " second remaining";
+      timeLeft--;
+    }
+    if (timeLeft === 0) {
+      timer.textContent = "Time's up! Your score is 0";
+      timer.style.margin = 0;
+      pause_timer();
+      showScore();
+      div1.style.display = "none";
+      div2.style.display = "none";
+      div3.style.display = "none";
+      div4.style.display = "none";
+      div5.style.display = "none";
+    }
+  }, 1000);
 }
 
+//retrives last stored item and displays as saved score
+function renderScoreboard() {
+  var initials = localStorage.getItem("initials");
+  var retrieveScore = localStorage.getItem("currentTime");
+  scoreDisplay.textContent = retrieveScore + " : " + initials;
+  finalScoreDiv.style.display = "none";
+  scoreboard.style.display = "flex";
+  retryBtn.style.display = "inline-block";
+}
+//shows start again button and hides score info
+function reveal() {
+  startAgainButton.style.display = "inline-block";
+  finalTime.style.display = "none";
+  finalScoreDiv.style.display = "none";
+  scoreboard.style.display = "none";
+  retryBtn.style.display = "none";
+}
+//starts timer over at 60 if user decides to try again
+function resetTimer() {
+  clearInterval(time);
+  timer.style.display = "inline-block";
+  timeLeft = 60;
+  timeLeft--;
+  timer.textContent = time + "seconds left";
+  if (time) {
+    renderScoreboard();
+  }
+  console.log("reset timer triggered");
+}
